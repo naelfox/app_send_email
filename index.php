aad9aaef2900dd106e7c49ff8c3dc99d21590e39
naelfox <natanaelvila2@gmail.com>
2022-09-04 16:44:42 -0300

    new files

--- index.php
+++ index.php
@@ -15,30 +15,30 @@
 		<div class="py-3 text-center">
 			<img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
 			<h2>Send Mail</h2>
-			<p class="lead">Seu app de envio de e-mails particular! </p>
+			<p class="lead">Your app to send emails! </p>
 		</div>
 
 		<div class="row">
 			<div class="col-md-12">
 
 				<div class="card-body font-weight-bold">
-					<form action="processa_envio.php" method="post">
+					<form action="sendProcess.php" method="post">
 						<div class="form-group">
-							<label for="para">Para</label>
-							<input type="text" class="form-control" id="para" placeholder="joao@dominio.com.br">
+							<label for="to">To</label>
+							<input type="text" name= "to"class="form-control" id="para" placeholder="john@domain.com.br">
 						</div>
 
 						<div class="form-group">
-							<label for="assunto">Assunto</label>
-							<input type="text" class="form-control" id="assunto" placeholder="Assundo do e-mail">
+							<label for="subject">Subject</label>
+							<input type="text" name="subject" class="form-control" id="assunto" placeholder="Subject of the email">
 						</div>
 
 						<div class="form-group">
-							<label for="mensagem">Mensagem</label>
-							<textarea class="form-control" id="mensagem"></textarea>
+							<label for="message">Message</label>
+							<textarea class="form-control" name="message" id="message"></textarea>
 						</div>
 
-						<button type="submit" class="btn btn-primary btn-lg">Enviar Mensagem</button>
+						<button type="submit" class="btn btn-primary btn-lg">Send Message</button>
 					</form>
 				</div>
 			</div>
