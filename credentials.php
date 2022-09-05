458bf2eaa335d88e2dba67f00b3758d179a4c4ff
naelfox <natanaelvila2@gmail.com>
2022-09-05 20:48:37 -0300

    readme

--- credentials.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Location: index.php');
-
-$host = 'host@host.com';                                            //Enable SMTP authentication
-$username  = 'email';                   
-$senha  = 'senha';                              
-
-
-?>
