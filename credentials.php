4b515abe5a1b2694cdf1581e762e6fe2352acc06
naelfox <natanaelvila2@gmail.com>
2022-09-05 20:42:52 -0300

    gitgnore

--- credentials.php
+++ credentials.php
@@ -1,10 +1,8 @@
 <?php
 header('Location: index.php');
 
-
 $host = 'host@host.com';                                            //Enable SMTP authentication
 $username  = 'email';                   
 $senha  = 'senha';                              
-
 
 ?>
